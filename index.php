5f72022444036e01ed835cb55d8617d89effef5b
stanley-blanco <44078216+stanley-blanco@users.noreply.github.com>
2018-10-11 21:12:44 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<body data-spy="scroll" data-target="#primary-menu">
+<head>
+    <?php include "secciones/head.php"; ?>
+    <?php include "secciones/menu.php" ?>
+    <?php include  "secciones/patrones.php" ?>
+    <?php include  "secciones/patronescod.php" ?>
+    <?php include  "secciones/patronesinfo.php" ?>
+    <?php include  "secciones/patronesurl.php" ?>
+    <?php include  "secciones/footer.php" ?>
+    <?php include "secciones/scripts.php" ?>
+    <?php include "secciones/modals.php" ?>
+</head>
+
+
+</body>
+</html>
